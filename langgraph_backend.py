82fd94a875ae15be63959104d72dd3367383a67b
Aryan Kumar <aryan1732003@gmail.com>
2026-04-07 12:31:56 +0530

    added sqlite backend

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -2,9 +2,11 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_mistralai import ChatMistralAI
-from langgraph.checkpoint.memory import InMemorySaver
+# from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -21,7 +23,9 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {'messages': [response]}
 
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False) # if database exist nhi krta hoga tho create ho jayega
+# check_same_thread se hum ek databse ko  multiple thread m access krr skte h but bydefault sqlite humko sirf 1 thread m kaam krne deta h
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node('chat_node', chat_node)
@@ -30,6 +34,10 @@ graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
+
+response = chatbot.invoke({'messages':[HumanMessage(content= 'what is my name?')]}, config = {'configurable':{'thread_id':'thread-2'}})
+
+print(response)
 
 # for message_chunk, metadata in stream:
 #     if message_chunk.content:
